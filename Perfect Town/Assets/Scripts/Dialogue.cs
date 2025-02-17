f244e56d498b0f66193e02c2f560fe70e850c75c
Will <wtbeaven@gmail.com>
2025-02-17 23:08:52 +1100

    Added dialogue script

--- Perfect Town/Assets/Scripts/Dialogue.cs
+++ Perfect Town/Assets/Scripts/Dialogue.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private string[] paragraph;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
